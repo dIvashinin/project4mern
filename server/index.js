b8cbc4b366eab84495c96481e4ecfba4b040408b
dIvashinin <dmitry.ivashinin@gmail.com>
2023-10-09 17:16:41 +0200

    established connection to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,40 @@
-import express from "express";
+import express, { json } from "express";
+import mongoose from "mongoose";
 // older notation
 // const express = require("express");
+import cors from "cors";
+import * as dotenv from "dotenv";
+dotenv.config();
+const router = express.Router();
 
 const app=express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({
+    extended: true,
+}));
+
+app.use("/api", router);
+router.get("/test", (req, res) =>{
+    res.json({
+       message: "test route", 
+    });
+});
+
+const DBConnection = async () => {
+    console.log('process.env.DB :>> ', process.env.DB);
+try {
+   await mongoose.connect(process.env.DB);
+   console.log('connection to MongoDB established :>> '); 
+} catch (error) {
+    console.log('error connecting to MongoDB :>> ');
+    
+}
+};
+DBConnection();
 
 app.listen(port, () => {
     console.log("server running in port:", port);
