356f9d371f0b763e627f9a13d4ffb5f4ffc9a021
dIvashinin <dmitry.ivashinin@gmail.com>
2023-10-12 11:52:23 +0200

    started params

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -1,6 +1,7 @@
 import productModel from "../models/productModel.js";
 
 const getAllProducts = async (req, res) =>{
+    // console.log('req :>> ', req);
     
     const allProducts = await productModel.find();
     // console.log('allProducts :>> ', allProducts);
