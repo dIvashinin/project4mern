ef192085297fae502fbc60041f254eaaf6ffd8eb
dIvashinin <dmitry.ivashinin@gmail.com>
2023-10-13 14:30:47 +0200

    soinstalled cloudinary

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,7 +1,8 @@
 
 
 const uploadImage = async (req, res) => {
-console.log("upload image controller working");
+// console.log("upload image controller working");
+console.log('req.file :>> ', req.file);
 };
 
 export {uploadImage};
--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -7,18 +7,18 @@ const storage = multer.diskStorage({});
 const fileFilter = (req, file, cb) => {
     // here goes logic t odecide if we accept file user is uploading or not
     console.log('file :>> ', file);
-    // const extension = path.extname();
-    // The function should call `cb` with a boolean
-    // to indicate if the file should be accepted
-    // To reject this file pass `false`, like so:
-    // cb(null, false);
-  
-    // To accept the file pass `true`, like so:
-    // cb(null, true);
-  
+    const extension = path.extname(file.originalname);
+    if (extension !== ".png" && extension !== ".jpg" && extension !== ".jpeg"){
+        // The function should call `cb` with a boolean
+        // to indicate if the file should be accepted
+        // To reject this file pass `false`, like so:
+        cb(null, false);
+    } else {
+        // To accept the file pass `true`, like so:
+        cb(null, true);
+    }
     // You can always pass an error if something goes wrong:
     // cb(new Error('I don\'t have a clue!'));
-  
   };
 
   const multerUpload = multer({ storage, fileFilter });
