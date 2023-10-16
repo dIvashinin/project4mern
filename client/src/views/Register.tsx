039ac21adac3adfc8579500c7c6757ce357d192d
dIvashinin <dmitry.ivashinin@gmail.com>
2023-10-16 11:19:52 +0200

    adding submit and upload, response via postman ok

--- client/src/views/Register.tsx
+++ client/src/views/Register.tsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 
 function Register() {
+
+    const [selectedFile, setSelectedFile] = useState<File | String>("")
+
+const handleFileInput = (e:ChangeEvent<HTMLInputElement>) => {
+console.log('e :>> ', e);
+//we need to satisfy typescript:
+setSelectedFile(e.target.files?.[0] || "");
+};
+    const handleFileSubmit = async (e:FormEvent<HTMLFormElement>) => {
+// to prevent refresh
+e.preventDefault();
+// then we use postman, send request, go to code snippet and copy JSFetch
+const formdata = new FormData();
+formdata.append("userImage", selectedFile);
+
+const requestOptions = {
+  method: 'POST',
+  body: formdata,
+//   redirect: 'follow'
+};
+
+try {
+    const response = await fetch("http://localhost:5001/api/users/imageUpload", requestOptions);
+    const result = await response.json();
+} catch (error) {
+    console.log('error :>> ', error);
+}
+    };
   return (
     <div>
         <h2>Register</h2>
         <div>
-            <form>
-                <input type="file" name="" id="" />
+            {/* first we do trick with e function and copy what type is this event */}
+            {/* then we change into our function name... */}
+            {/* <form onSubmit={(e)=>}> */}
+            <form onSubmit={handleFileSubmit}>
+                <input type="file" name="file" id="file" onChange={handleFileInput} />
+                <button type='submit'>upload image</button>
             </form>
         </div>
     </div>
-  )
+  );
 }
 
 export default Register;
--- server/controller/userController.js
+++ server/controller/userController.js
@@ -9,6 +9,10 @@ if (req.file) {
     try {
         const uploadedImage = await cloudinary.uploader.upload(req.file.path, {folder:"project4mern"});
         console.log('uploadedImage :>> ', uploadedImage);
+        res.status(200).json({
+            message: "image uploaded successfully",
+            userImage: uploadedImage.secure_url,
+        });
         // if uploadedImage is successful (returns a valid object), save that URL into the user collection
     } catch (error) {
        console.log('error :>> ', error); 
