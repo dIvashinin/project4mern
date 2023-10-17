b6facc5b029fdd3e009d4600b5faa62b202cbc54
dIvashinin <dmitry.ivashinin@gmail.com>
2023-10-17 11:26:52 +0200

    completed spike, we can now register new user and see result in console and DB

--- client/src/views/Register.tsx
+++ client/src/views/Register.tsx
@@ -51,9 +51,36 @@ try {
 }
     };
 
-const handleRegisterInput = () => {
+const handleRegisterInput = (e:ChangeEvent<HTMLInputElement>) => {
+//trick here. now we see what field we are using, so it saves us lines of code
+console.log('e.target.name :>> ', e.target.name);
+//now this is gonna be dynamic - it takes the field we are typing in inside [e.target.name]
+    setNewUser({...newUser,[e.target.name]:e.target.value })
+};
 
+const handleSubmitRegister = async (e:FormEvent<HTMLFormElement>) => {
+e.preventDefault();
+//here we copy our fetch function from postman
+const urlencoded = new URLSearchParams();
+urlencoded.append("userName", newUser.userName);
+urlencoded.append("email", newUser.email);
+urlencoded.append("password", newUser.password);
+urlencoded.append("userImage", newUser.userImage ? newUser.userImage : "https://i.pinimg.com/474x/13/0f/96/130f9601ce0b948996e13bc2b1d88a66.jpg");
+
+const requestOptions = {
+  method: 'POST',
+  body: urlencoded,
+  
+};
+
+try {
+    const response = await fetch("http://localhost:5001/api/users/register", requestOptions);
+    const result = await response.json();
+    console.log('result :>> ', result);
+} catch (error) {
+   console.log('error :>> ', error); 
 }
+};
 
 
   return (
@@ -61,14 +88,14 @@ const handleRegisterInput = () => {
         <h2>Register</h2>
         <div>
             <div>
-                <form className='input-form'>
+                <form className='input-form' onSubmit={handleSubmitRegister}>
                    <input type="text" name="userName" id="userName" onChange={handleRegisterInput}/>
                    <label htmlFor="userName">user name</label>
                    <input type="text" name="email" id="email" onChange={handleRegisterInput}/>
                    <label htmlFor="email">email</label>
                    <input type="text" name="password" id="password" onChange={handleRegisterInput}/>
                    <label htmlFor="password">password</label>
-
+                    <button type='submit'>register</button>
                 </form>
             </div>
 
