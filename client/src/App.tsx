bd4b3600283e89f6c25632ad4b3ecee15540cca7
dIvashinin <dmitry.ivashinin@gmail.com>
2023-10-24 23:22:50 +0200

    some css changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,9 @@ import {
   createBrowserRouter,
   Route,
   RouterProvider,
-//  Link,
+  //  Link,
   Outlet,
 } from "react-router-dom";
-
 
 // import { useEffect, useState } from "react";
 import Login from "../views/Login";
@@ -18,78 +17,73 @@ import Products from "../views/Products";
 import ErrorPage from "../views/ErrorPage";
 import Details from "../views/Details";
 
-
 // import './App.css';
 
 function App() {
-
   //router
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Root/>} errorElement={<ErrorPage/>}>
-      {/* <Route index element = {<Home/>} /> */}
-      <Route path="products" element = {<Products/>} />
-      <Route path="products/:details" element = {<Details/>} />
-      <Route path="about" element = {<About/>} />
-      <Route path="register" element = {<Register/>} />
-      <Route path="login" element = {<Login/>} />
-      {/* <Route path="*" element = {<ErrorPage/>} /> */}
-    </Route>
-  )
-);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+        {/* <Route index element = {<Home/>} /> */}
+        <Route path="products" element={<Products />} />
+        <Route path="products/:details" element={<Details />} />
+        <Route path="about" element={<About />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        {/* <Route path="*" element = {<ErrorPage/>} /> */}
+      </Route>
+    )
+  );
 
+  //   const [isUserLogged, setIsUserLogged] = useState(false);
+  //   const isUserLoggedIn = () => {
+  //     const token = localStorage.getItem("token");
+  //     //or shorter in one line what's below
+  //     return token ? true : false
+  //     // if (token){
+  //     //     return true
+  //     // }else {
+  //     //    return false
+  //     // }
+  // };
 
+  // const logout = () => {
+  //   localStorage.removeItem("token");
+  //   setIsUserLogged (false);
+  // }
 
-  
-//   const [isUserLogged, setIsUserLogged] = useState(false);
-//   const isUserLoggedIn = () => {
-//     const token = localStorage.getItem("token");
-//     //or shorter in one line what's below
-//     return token ? true : false
-//     // if (token){
-//     //     return true
-//     // }else {
-//     //    return false 
-//     // }
-// };
-
-// const logout = () => {
-//   localStorage.removeItem("token");
-//   setIsUserLogged (false);
-// }
-
-// useEffect(() => {
-//   const isLoggedIn = isUserLoggedIn();
-//   if (isLoggedIn) {
-//     console.log("user is logged in");
-//     setIsUserLogged(true);
-//   }else {
-//     console.log("user is not logged in");
-//     setIsUserLogged(false);
-//   }
-// }, [isUserLogged]);
+  // useEffect(() => {
+  //   const isLoggedIn = isUserLoggedIn();
+  //   if (isLoggedIn) {
+  //     console.log("user is logged in");
+  //     setIsUserLogged(true);
+  //   }else {
+  //     console.log("user is not logged in");
+  //     setIsUserLogged(false);
+  //   }
+  // }, [isUserLogged]);
 
   return (
     <>
-     <hr />
-     
-     <RouterProvider router={router}/>
-     <Outlet/>
-     <h1>Store</h1> 
-    {/* <button onClick={logout}>logout</button> */}
-     <hr />
-     {/* <Register/> */}
-     <hr />
-     {/* <Login/> */}
-     <hr />
+      <hr />
+
+      <RouterProvider router={router} />
+      <Outlet />
+      <h1>Store</h1>
+      {/* <button onClick={logout}>logout</button> */}
+      <hr />
+      {/* <Register/> */}
+      <hr />
+      {/* <Login/> */}
+      <hr />
     </>
   );
 }
 const Root = () => {
   return (
     <>
-    <Navbar/>
-    <Outlet/>
+      <Navbar />
+      <Outlet />
     </>
   );
 };
--- client/views/Products.tsx
+++ client/views/Products.tsx
@@ -1,31 +1,29 @@
 import { useState } from "react";
 
 function Products() {
+  const [inputText, setInputText] = useState("");
 
-    const [inputText, setInputText] = useState("");
-
-    const inputChangeHandler = (event) => {
-        console.log("event.target.value :>> ", event.target.value);
-        const text = event.target.value;
-        setInputText(text);
-      };
+  const inputChangeHandler = (event) => {
+    console.log("event.target.value :>> ", event.target.value);
+    const text = event.target.value;
+    setInputText(text);
+  };
 
   return (
     <div>
-        <h2>Products</h2>
+      <h2>Products</h2>
 
-        <div className="searchbar">
+      <div className="searchbar">
         <input
-            id="mySearchInput"
+          id="mySearchInput"
           className="search-input"
           type="text"
           placeholder="search me..."
           onChange={inputChangeHandler}
         />
-      </div> 
-
+      </div>
     </div>
-  )
+  );
 }
 
 export default Products;
