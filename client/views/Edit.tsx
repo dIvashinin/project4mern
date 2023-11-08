09a6dc98c8e006f12f193324622c96d6170fd7fd
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-08 18:31:21 +0100

    started to work on update front end functionality

--- client/views/Edit.tsx
+++ client/views/Edit.tsx
@@ -1,14 +1,34 @@
 // import React from 'react'
 // import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
-function EditPage() {
+const EditPage = () => {
+    let {id} = useParams();
+    const [blog, setBlog] = useState({
+        description: "",
+    userName: "",
+    email: "",
+    brand: "",
+    });
+    const getBlog = async () => {
+     const response = await fetch(`http://localhost:5001/api/posts/all/${id}`);
+     console.log('response :>> ', response);
+    //     setBlog({
+    //         description: response.description,
+    // userName: "",
+    // email: "",
+    // brand: "", 
+    //     })
+    }
+
   return (
     <div> 
       {/* <h2>Home</h2>
       <h1 className="home-about-firstline">This is about Berlin.</h1> */}
-     <h3> edit</h3>
+     <h3> edit </h3>
       <h2> edit</h2>
-      <h1> edit</h1>
+      <h1> edit {id}</h1>
       {/* <NavLink to="/posts">
         <img
           className="eingang-picture"
