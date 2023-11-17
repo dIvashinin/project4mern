51fec9f5da20915118257a93939e484830ffd9ce
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-17 10:18:35 +0100

    added jwtAuth middleware

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -8,12 +8,13 @@ import {
 } from "../controller/userController.js";
 
 import multerUpload from "../middlewares/multer.js";
+import jwtAuth from "../middlewares/jwtAuth.js";
 const router = express.Router();
 
 router.post("/imageUpload", multerUpload.single("userImage"), uploadImage);
 router.post("/register", register);
 router.post("/login", login);
 router.get("/all", getAllUsers);
-router.get("/profile", getProfile);
+router.get("/profile",jwtAuth , getProfile);
 
 export default router;
