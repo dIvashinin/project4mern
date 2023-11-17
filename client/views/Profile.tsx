4adc9769a7f1f5c98812efc3b1f873859834e8f0
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-17 11:53:55 +0100

    so profile is working in console, i am able to see user after login in

--- client/views/Profile.tsx
+++ client/views/Profile.tsx
@@ -1,10 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+type User = {
+    userName: string;
+    email: string;
+    userImage: string;
+};
+
+const getProfile = async () => {
+//in header which is not visible to user, we put sensible info
+//token is stored in local storage
+// so we go there and take it
+const token = localStorage.getItem("token");
+if (!token) {
+    alert ("you need to login first");
+    //either warn user or redirect to login
+}
+if(token) {
+
+    //send request
+    // const myHeaders = new Headers();
+    // myHeaders.append(
+        
+    //     "Authorization",
+        //careful with syntax!
+    //  `Bearer ${token}`);
+   
+   
+     const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+    };
+    
+    fetch("http://localhost:5001/api/users/profile", requestOptions)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+    
+
+
+
+    // const requestOptions = {
+    //     'Content-Type': 'application/json',
+    //     method: 'GET',
+    //     // redirect: 'follow'
+    //   };
+      
+    //   try {
+    //     const response = await fetch("http://localhost:5001/api/users/profile", 
+    //     requestOptions);
+    //     if (response.ok) {
+    //        const result = await response.json();
+    //        console.log('result :>> ', result);
+    //     }
+    //   } 
+    //   catch (error) {
+        
+    //   }
+        // .then(response => response.text())
+        // .then(result => console.log(result))
+        // .catch(error => console.log('error', error));
+ }
+};
 
 function Profile() {
+    const [user, setUser] = useState<User | null>(null)
+
   return (
     <div>
         <h2>Profile</h2>
-        <button>get Profile</button>
+        <button onClick={getProfile}>get profile</button>
         
         </div>
   );
