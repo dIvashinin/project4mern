7ab0fcbe156ed1dc2aa142e80b0a1aaeaf267520
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-17 10:38:55 +0100

    modified findById as async await, user is found with a token

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,5 +1,6 @@
 import {ExtractJwt, Strategy as JwtStrategy} from "passport-jwt";
 import * as dotenv from "dotenv";
+import userModel from "../models/userModel.js";
 dotenv.config();
 
 // console.log('process.env.SECRET :>> ', process.env.SECRET);
@@ -9,18 +10,38 @@ const opts = {
 };
 
 //1.Define Strategy
-const jwtPassportStrategy = new JwtStrategy(opts, function(jwt_payload, done) {
-    User.findOne({id: jwt_payload.sub}, function(err, user) {
-        if (err) {
-            return done(err, false);
-        }
+const jwtPassportStrategy = new JwtStrategy(opts, async function(jwt_payload, done) {
+
+    //findOne method which we had in documentation is no longer valid so we need to rebuild it
+    
+    try {
+        const user = await userModel.findById(jwt_payload.sub)
+    
         if (user) {
-            return done(null, user);
+            console.log('user found');
+            return done(null, user);   
         } else {
-            return done(null, false);
-            // or you could create a new account
+            return done(null, false); 
         }
-    });
+        
+    } catch (error) {
+        console.log(' passport err block ');
+        return done(err, false); 
+    }
+    
+
+
+    // userModel.findOne({id: jwt_payload.sub}, function(err, user) {
+    //     if (err) {
+    //         return done(err, false);
+    //     }
+    //     if (user) {
+    //         return done(null, user);
+    //     } else {
+    //         return done(null, false);
+    //         // or you could create a new account
+    //     }
+    // });
 });
 //2.use this strategy with passport
 
